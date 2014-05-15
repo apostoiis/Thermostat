3b4191d122ced54da41c5a7b18e1988f87e61b90
Sroop <sroopsunar@hotmail.com>
2014-05-15 13:52:11 +0100

    for power save mode on, thermostat has a max and min temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -4,13 +4,25 @@ describe("Thermostat", function() {
 
   it('has a default temperature of 18 degrees', function() {
     expect(thermostat.temperature).toEqual(18);
-
   })
 
   it('has power save mode setting switched on as a default', function() {
     expect(thermostat.powerSaveMode).toEqual(true);
+  })
+
+  describe('If power save mode is on', function(){
+
+      it('has a maximum temperature of 25', function() {
+        expect(thermostat.maximumTemperature()).toEqual(25);
+      })
+
+      it('has a minimum temperature of 12', function() {
+        expect(thermostat.minimumTemperature()).toEqual(12);
+      })
 
   })
+
+
 
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,3 +2,12 @@ function Thermostat() {
 	this.temperature = 18;
 	this.powerSaveMode = true;
 };
+
+Thermostat.prototype.maximumTemperature = function() {
+	return this.temperature = 25;
+}
+
+Thermostat.prototype.minimumTemperature = function() {
+	return this.temperature = 12;
+}
+
